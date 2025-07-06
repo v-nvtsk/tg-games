78e4314b53d39394a37efbdbfe2802cfef052ede
v-nvtsk <v@nvtsk.ru>
2025-07-06 20:55:54 +0300

    fix: swipe

--- src/2048/hooks/use-game-input.tsx
+++ src/2048/hooks/use-game-input.tsx
@@ -1,12 +1,11 @@
-// src/2048/hooks/use-game-input.ts
+// src/2048/hooks/use-game-input.tsx
 import { useEffect } from "react";
-import { useGameActions } from "./use-game-actions"; // Предполагаем, что useGameActions находится рядом
+import { useGameActions } from "./use-game-actions";
 
 export const useGameInput = () => {
   const { moveLeft, moveRight, moveUp, moveDown } = useGameActions();
 
   useEffect(() => {
-    // --- Обработка клавиатуры ---
     const keyListener = (e: KeyboardEvent) => {
       switch (e.key) {
       case "ArrowLeft":
@@ -34,8 +33,17 @@ export const useGameInput = () => {
     let touchEndY = 0;
 
     const handleTouchStart = (e: TouchEvent) => {
+      e.preventDefault();
       touchStartX = e.touches[0].clientX;
       touchStartY = e.touches[0].clientY;
+    };
+
+    const handleTouchMove = (e: TouchEvent) => {
+      e.preventDefault();
+      if (e.touches.length > 0) {
+        touchEndX = e.touches[0].clientX;
+        touchEndY = e.touches[0].clientY;
+      }
     };
 
     const handleTouchEnd = (e: TouchEvent) => {
@@ -48,7 +56,6 @@ export const useGameInput = () => {
       const minSwipeDistance = 30;
 
       if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > minSwipeDistance) {
-
         if (dx > 0) {
           moveRight();
         } else {
@@ -61,14 +68,20 @@ export const useGameInput = () => {
           moveUp();
         }
       }
+      touchStartX = 0;
+      touchStartY = 0;
+      touchEndX = 0;
+      touchEndY = 0;
     };
 
-    document.addEventListener("touchstart", handleTouchStart, { passive: true });
-    document.addEventListener("touchend", handleTouchEnd, { passive: true });
+    document.addEventListener("touchstart", handleTouchStart, { passive: false });
+    document.addEventListener("touchmove", handleTouchMove, { passive: false });
+    document.addEventListener("touchend", handleTouchEnd, { passive: false });
 
     return () => {
       document.removeEventListener("keydown", keyListener);
       document.removeEventListener("touchstart", handleTouchStart);
+      document.removeEventListener("touchmove", handleTouchMove);
       document.removeEventListener("touchend", handleTouchEnd);
     };
   }, [moveLeft, moveRight, moveUp, moveDown]);
