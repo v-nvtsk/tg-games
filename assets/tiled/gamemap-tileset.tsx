543b30902c5876b9d288cc658976fa2679793a61
v-nvtsk <v-nvtsk@users.noreply.github.com>
2025-07-16 05:49:07 +0000

    Deploying to gh-pages from @ v-nvtsk/tg-games@465aa92153206234d5d9110e73c57b4767492ca5 🚀

---
+++ assets/tiled/gamemap-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="gamemap-tileset" tilewidth="128" tileheight="128" tilecount="851" columns="37">
+ <image source="../map.png" width="4736" height="2944"/>
+</tileset>
