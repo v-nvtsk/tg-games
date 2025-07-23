fe19447217de35fe4f4aa406b7f6e20eb5c55470
v-nvtsk <v@nvtsk.ru>
2025-07-24 00:35:04 +0300

    feat(games): add 2048

---
+++ src/features/game-2048/2048/hooks/index.ts
@@ -0,0 +1,11 @@
+export * from "./use-game";
+
+export * from "./use-game-actions";
+
+export * from "./use-field";
+
+export * from "./use-game-input";
+
+export * from "./use-game-over";
+
+export * from "./use-score";
