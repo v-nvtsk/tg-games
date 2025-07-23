dd375e659bec956084590d04c4dbcf035db32730
v-nvtsk <v@nvtsk.ru>
2025-07-24 00:36:48 +0300

    refactor(app): move useTelegram inside useAuth hook

--- src/core/hooks/use-auth/use-auth.ts
+++ src/core/hooks/use-auth/use-auth.ts
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
 import { useAuthStore } from "@core/state";
+import { useTelegram } from "../use-telegram";
 
 export const useAuth = () => {
+  const { webApp: { initData } } = useTelegram();
+
   const { isAuthenticated, token, authenticateUser } = useAuthStore();
 
   useEffect(() => {
-    if (!isAuthenticated) {
+    if (!isAuthenticated && initData) {
       void authenticateUser();
     }
-  }, [isAuthenticated, authenticateUser]);
+  }, [isAuthenticated, authenticateUser, initData]);
 
   return token;
 };
